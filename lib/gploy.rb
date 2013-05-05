a5debe1cbf7285e931cf43c6f8ee2681f9d03e6c
Edipo L Federle <edipofederle@gmail.com>
2013-05-05 11:55:11 -0300

    remove files

---
+++ lib/gploy.rb
@@ -0,0 +1,22 @@
+$:.unshift File.dirname(__FILE__) # For use/testing when no gem is installedr"
+
+def require_all(path)
+  glob = File.join(File.dirname(__FILE__), path, '*.rb')
+  Dir[glob].each do |f|
+    require f
+  end
+end
+
+
+require "rubygems"
+require 'net/ssh'
+require 'fileutils'
+require 'yaml'
+require 'net/sftp'
+
+require 'gploy/helpers'
+require 'gploy/configure'
+require 'gploy/Settings'
+require 'gploy/deep_symbolize'
+
+require 'gploy/version'
